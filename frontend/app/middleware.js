97005be20f77575cd8343fe7260ba3fc796296bf
majidkawouk <57314886+majidkawouk@users.noreply.github.com>
2025-08-12 16:27:05 +0300

    Update middleware.js

--- frontend/app/middleware.js
+++ frontend/app/middleware.js
@@ -1,19 +1,18 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const role = req.cookies.get("role")?.value; // Read role from cookies
-
+  const role = req.cookies.get("role")?.value;
   if (req.nextUrl.pathname.startsWith("/tasks/admin")) {
-    // If user is not admin, redirect to login
+   
     if (role !== "admin") {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }
 
-  return NextResponse.next(); // Allow access
+  return NextResponse.next(); 
 }
 
-// Apply middleware only to specific routes
+
 export const config = {
   matcher: ["/tasks/admin"],
 };
